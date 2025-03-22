7f7b490a9bbb21f92806b080a1b719befaabd50b
Nasir_Mehmood <nasirjani757571@gmail.com>
2025-03-22 15:03:21 +0500

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def get_fb_links():
         
         results = [
             # {"link": row[1], "page_name": row[0], "post_image": row[4], "timestamp": row[2] if row[2] else None}
-            # {"link": row[1], "page_name": row[0], "post_image": row[3], "timestamp": row[2] if row[2] else None}
+            {"link": row[1], "page_name": row[0], "timestamp": row[2] if row[2] else None}
 
             for row in data
         ]
