205400600f485bdd101c11086664e8f1622f8dba
Nasir_Mehmood <nasirjani757571@gmail.com>
2025-04-10 12:07:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 app.secret_key = "your_secret_key"  # Required for session management
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://instaxrss_user:QGBb5ALqiBraZtjt1c1zoifa4Kf4G1Tu@dpg-cv7sqcqj1k6c739htp00-a.oregon-postgres.render.com/instaxrss")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://neondb_owner:npg_7SjyKhDinEv8@ep-young-term-a5zyo5in-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require")
 
 # Hardcoded username and password
 USERNAME = "IMM"
